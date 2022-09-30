6f8ab63ce293730b0d29d65b43bbf80d19ce5acf
fikriibrahimahid <fikriibrahim1999@gmail.com>
2022-09-30 14:11:36 +0700

    create initial cmd/main/main.go file

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/fikriibrahimahid/fcc-gomux-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":9010", r))
+}
